e8a699b0901ae67d2df569a2d20dbc5243b09b51
Carl X. Su <bcbcarl@gmail.com>
2016-07-09 16:25:05 +0800

    Update license to MIT

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject brave-clojure-ch4 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"
+            :year 2016
+            :key "mit"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
